1281fa7bd98cf7820d0b723d159ea151d3692558
ingar <muhammadingar05@gmail.com>
2024-09-27 14:02:02 +0100

    nav bar style

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,15 @@ import three from "./assets/IMG_2635.jpeg";
 import four from "./assets/IMG_3572.jpeg";
 import five from "./assets/IMG_3681.jpeg";
 import ContactForm from './ContactForm.jsx';
+import rari2 from "./assets/rari2.jpeg";
 
 function App() {
-  const featurableWidgetId= "API KEY"
+  const featurableWidgetId= "ab151222-7f6e-489a-b45a-63b8500fb10a"
   const foundedText = "Founded in 2023, Pristine Auto Care is a dynamic and innovative car detailing company that offers both mobile services and convenient site locations around Stoneygate off London Road and in Evington off Uppingham Road. We provide a comprehensive range of detailing services, including exterior 3-step washes, iron decontamination, clay bar treatments, machine and hand polishing, interior decontamination, steam cleaning, leather care and restoration, deep seat cleaning, ceramic coating, headlight restoration, and more.<br /> At Pristine Auto Care, we cater to all vehicle owners who value the importance of maintaining their cars health and appearance. We believe that your vehicle is a reflection of you, and we are dedicated to ensuring it stays in top condition. Our commitment to delivering excellent service at competitive prices, combined with our attention to detail, sets us apart in the industry. <br />As a company founded by two young entrepreneurs, we are constantly seeking innovative ways to enhance our services and stand out in the market we work as a pair too help multitask and detail your car at a much more reasonable time. Our mission is to provide exceptional care for your car, making sure every detail counts. Trust Pristine Auto Care to keep your vehicle looking its best."
   const  whyUsText= "At PristineAutoCare, we combine knowledge and expertise with the latest detailing techniques to deliver exceptional care for your vehicle. As skilled professionals we use top-quality products and high quality equipment to ensure every detail is meticulously attended to, leaving your car spotless and well-protected. We're committed to your satisfaction and take pride in exceeding your expectations with every service."
   const whatWeDo = "we excel"
   const [product, setProduct] = useState([{name:"Premium Detail",link:"https://google.com"},{name:"Paint Correction",link:"https://google.com"},{name:"motorbike Detail",link:"https://google.com"}])
-  
+
   const packages = useRef(null)
   const contact = useRef(null)
 
@@ -30,17 +31,21 @@ function App() {
     })
   }
   return (
-    // <ContactForm></ContactForm>
-    <div>
+    <body className="flex flex-col">
+    {/* <ContactForm></ContactForm> */}
 
     <NavBar scrollToSection={scrollToSection} packages={packages} />
-    <Gallery images={[{original:one},{original:three},{original:four},{original:two},{original:five}]}/>
-     <div id={"reasons"}>
+    {/* <Gallery images={[{original:one},{original:three},{original:four},{original:two},{original:five}]}/>
+    <div className="reasons" id={"reasons"}>
+      <div>
         <h2>Why Us?</h2>
         <p>{whyUsText}</p>
       </div>
+        <div> <img src={one} alt={"rari"}></img>
+      </div>
+    </div>
       <div id='reviews'>
-      <ReactGoogleReviews carouselSpeed={10000} theme="dark" layout="carousel" featurableId={featurableWidgetId} />
+      <ReactGoogleReviews carouselSpeed={10000} layout="carousel" featurableId={featurableWidgetId} />
       </div>
       <div id={"founded"}  >
         <p dangerouslySetInnerHTML={{ __html: foundedText }}></p>
@@ -54,9 +59,9 @@ function App() {
           {product.map((item,index) => {if(index %2 === 0){return(<a href={item.link}><td>{item.name}</td></a>)} else{return (<><a href={item.link}><td>{item.name}</td></a><tr></tr></>)}})}
           </tbody>
         </table>
-      </div>
+      </div> */}
     <Footer/>
-    </div>
+    </body>
   )
 }
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,22 +1,13 @@
 import logo from "./assets/logo.jpeg"
 export default function NavBar({scrollToSection,packages}){
     return(<>
-        <style>{
-           `.navElement{display:flex;flex-Direction:row;justify-content:centre;align-Items:center;}`
-            }
-        </style>
-        <nav style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
-            <div style={{display:"flex",flexDirection:"row",justifyContent:"end"}}>
-            <img style={{height:"15vh"}} src={logo} alt="Logo"/>
-            </div>
-            <h1 style={{display:"flex", flexDirection:"row",justifyContent:"centre",alignItems:"center"}}>Pristine Auto Care</h1>
 
-            <ul style={{display:"flex", flexDirection:"row",justifyContent:"space-between",width:"70%"}}>
-                <h2 className="navElement" onClick={()=>scrollToSection(packages)}>Detailing Packages</h2>
-                <h2 className="navElement">Contact</h2>
-                <h2 className="navElement" >Motorbike Packages</h2>
-                <h2 className="navElement">Quick Quote</h2>
-            </ul>
+        <nav className="flex flex-row justify-between h-32  place-items-center bg-black	 ">
+            <div className="flex-1  flex justify-center"><img className="h-28"  src={logo} alt="Logo"/></div>
+                <h2 className="flex-1 flex justify-center text-white hover:text-lg hover:cursor-pointer " onClick={()=>scrollToSection(packages)}>Detailing Packages</h2>
+                <h2 className="flex-1 flex justify-center text-white hover:text-lg hover:cursor-pointer">Contact</h2>
+                <h2 className="flex-1 flex justify-center text-white hover:text-lg hover:cursor-pointer" >Motorbike Packages</h2>
+                <h2 className="flex-1 flex justify-center text-white hover:text-lg hover:cursor-pointer">Quick Quote</h2>
         </nav>
         </>
     )
