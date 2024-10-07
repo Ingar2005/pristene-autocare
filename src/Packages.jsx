63aacee1c2cd676020772815fd2e2399da4c2bd3
ingar2005 <muhammadingar05@gmail.com>
2024-10-07 21:53:58 +0100

    fix padding , too much

--- src/Packages.jsx
+++ src/Packages.jsx
@@ -3,7 +3,7 @@ import rari2 from "./assets/rari2.jpeg";
 import PackageBox from "./PackageBox.jsx";
 export default function Packages(){
 
-  return(<div className="flex flex-row justify-around flex-wrap lg:mx-44 pt-24">
+  return(<div className="flex flex-row justify-around flex-wrap lg:mx-24 pt-24">
   <PackageBox title="prem package" price="£10K" description={["- one","- two","- three"]} image={rari2} />
   <PackageBox title="prem package" price="£10K" description={["- one","- two","- three"]} image={rari2} />
   <PackageBox title="prem package" price="£10K" description={["- one","- two","- three"]} image={rari2} />
