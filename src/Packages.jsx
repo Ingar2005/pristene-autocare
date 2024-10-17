b77b5aa5bb121495e2ebf2d97d6bda532396b210
ingar2005 <muhammadingar05@gmail.com>
2024-10-17 17:07:19 +0100

    fix phone bug

--- src/Packages.jsx
+++ src/Packages.jsx
@@ -6,16 +6,18 @@ export default function Packages(){
   return(<div className="flex flex-row justify-around flex-wrap lg:mx-24 pt-24">
 
 
-    <div className="mx-7 flex flex-col align-middle justify-center h-[27rem] w-screen border-black border-2 my-8">
-    <div className="flex mx-auto w-[27rem]"><img className="" src={rari2} alt="maintanance"></img></div>
-    <div className="flex flex-col justify-center items-center my-8"><h2 className="font-bold">Ceramic Coating (From £100k)</h2>
-     <p>- one <br /> </p>
-     <p>- two <br /> </p>
-     <p>- three <br /> </p>
-
-    </div>
-    </div>
-  <PackageBox title="Maintenance Scheme" price="£10K" description={["- one","- two","- three"]} image={rari2} />
+   <div className="mx-3 flex flex-col align-middle justify-center h-[27rem] w-screen border-black border-2 my-8 ">
+  <div className="flex mx-auto px-5  max-w-full justify-center">
+    <img className="w-full max-w-[30rem] h-auto object-contain" src={rari2} alt="maintenance" />
+  </div>
+  <div className="flex flex-col justify-center items-center my-5">
+    <h2 className="font-bold">Ceramic Coating (From £100k)</h2>
+    <p>- one <br /></p>
+    <p>- two <br /></p>
+    <p>- three <br /></p>
+  </div>
+</div>
+ <PackageBox title="Maintenance Scheme" price="£10K" description={["- one","- two","- three"]} image={rari2} />
   <PackageBox title="Standerd Detail" price="£10K" description={["- one","- two","- three"]} image={rari2} />
   <PackageBox title="Pain Correction" price="£10K" description={["- one","- two","- three"]} image={rari2} />
  <PackageBox title="Deep Clean" price="£10K" description={["- one","- two","- three"]} image={rari2} />
